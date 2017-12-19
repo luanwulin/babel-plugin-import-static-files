c51c1080672540df2600f66288569f8ee2c50216
linnanxuan <linnanxuan@zbj.com>
2017-12-19 11:11:48 +0800

    path join

--- src/transform.js
+++ src/transform.js
@@ -3,64 +3,64 @@ import fs from 'fs-extra';
 import path from 'path';
 
 function getHash(str) {
-  return crypto
-    .createHash('sha1')
-    .update(str, 'utf8')
-    .digest('hex')
-    .slice(0, 8);
+    return crypto
+        .createHash('sha1')
+        .update(str, 'utf8')
+        .digest('hex')
+        .slice(0, 8);
 }
 
 function getFile(state, absPath, opts) {
-  const root = (opts.replaceExtendDir ? `${state.file.opts.sourceRoot}${opts.replaceExtendDir}` : state.file.opts.sourceRoot) || process.cwd();
-  let file = absPath.replace(root, '');
+    const root = (opts.replaceExtendDir ? path.join(state.file.opts.sourceRoot, opts.replaceExtendDir) : state.file.opts.sourceRoot) || process.cwd();
+    let file = absPath.replace(root, '');
 
-  if (opts.baseDir) {
-    file = path.join(opts.baseDir, file);
-    fs.copySync(absPath, path.join(root, file))
-  }
+    if (opts.baseDir) {
+        file = path.join(opts.baseDir, file);
+        fs.copySync(absPath, path.join(root, file))
+    }
 
-  return '/' + file
-    .replace(/\\/g, '/')
-    .replace(/\/{2,}/g, '/')
-    .replace(/^\/+/g, '');
+    return '/' + file
+        .replace(/\\/g, '/')
+        .replace(/\/{2,}/g, '/')
+        .replace(/^\/+/g, '');
 }
 
 const getVariableName = (p) => {
-  if (
-    p.node.specifiers
-    && p.node.specifiers[0]
-    && p.node.specifiers[0].local
-  ) {
-    return p.node.specifiers[0].local.name
-  }
+    if (
+        p.node.specifiers
+        && p.node.specifiers[0]
+        && p.node.specifiers[0].local
+    ) {
+        return p.node.specifiers[0].local.name
+    }
 }
 
 export default (p, t, state, opts, absPath, calleeName) => {
-  const file = getFile(state, absPath, opts);
-  let hash = '';
+    const file = getFile(state, absPath, opts);
+    let hash = '';
 
-  if (opts.hash === 1) {
-    const content = fs.readFileSync(absPath, 'utf8').trim();
-    hash = '?' + getHash(content);
-  }
+    if (opts.hash === 1) {
+        const content = fs.readFileSync(absPath, 'utf8').trim();
+        hash = '?' + getHash(content);
+    }
 
-  const uri = `${opts.baseUri || ''}${file}${hash}`;
+    const uri = `${opts.baseUri || ''}${file}${hash}`;
 
-  if (calleeName === 'require') {
-    p.replaceWith(t.StringLiteral(uri));
-    return;
-  }
+    if (calleeName === 'require') {
+        p.replaceWith(t.StringLiteral(uri));
+        return;
+    }
 
-  const variableName = getVariableName(p);
-  if (variableName) {
-    p.replaceWith(
-      t.variableDeclaration('const', [
-        t.variableDeclarator(
-          t.identifier(variableName),
-          t.stringLiteral(uri)
-        )
-      ])
-    );
-  }
+    const variableName = getVariableName(p);
+    if (variableName) {
+        p.replaceWith(
+            t.variableDeclaration('const', [
+                t.variableDeclarator(
+                    t.identifier(variableName),
+                    t.stringLiteral(uri)
+                )
+            ])
+        );
+    }
 
 }
