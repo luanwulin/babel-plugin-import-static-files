352232b74dcef9b9c3d851bd161cdf4527102a0d
linnanxuan <linnanxuan@zbj.com>
2018-01-04 10:47:38 +0800

    他的版本

--- src/index.js
+++ src/index.js
@@ -1,51 +1,56 @@
-import {dirname, extname, resolve} from'path';
+import path, { dirname, extname, resolve } from 'path';
 import transform from './transform';
 
 export const defaultOptions = {
-  baseDir: '/static',
-  extensions: [
-    '.gif',
-    '.jpeg',
-    '.jpg',
-    '.png',
-    '.svg'
-  ]
+    baseDir: '/static',
+    hash: false,
+    extensions: [
+        '.gif',
+        '.jpeg',
+        '.jpg',
+        '.png',
+        '.svg'
+    ]
 }
 
 const applyTransform = (p, t, state, value, calleeName) => {
-  const ext = extname(value);
-  const options = Object.assign({}, defaultOptions, state.opts);
+    const ext = extname(value);
+    let options = Object.assign({}, defaultOptions, state.opts);
 
 
-  if (options.extensions && options.extensions.indexOf(ext) >= 0) {
-    const dir = dirname(resolve(state.file.opts.filename));
-    const absPath = resolve(dir, value);
+    if (options.extensions && options.extensions.indexOf(ext) >= 0) {
+        const dir = dirname(resolve(state.file.opts.filename));
+        const absPath = resolve(dir, value);
 
-    transform(p, t, state, options, absPath, calleeName);
-  }
+        if (options.baseDir) {
+            options.baseDir = options.baseDir.replace(/[\/\\]+/g, path.sep);
+        }
+
+        transform(p, t, state, options, absPath, calleeName);
+    }
 }
 
-export function transformImportsInline({types: t}) {
-  return {
-    visitor: {
-      ImportDeclaration(p, state) {
-        applyTransform(p, t, state, p.node.source.value, 'import');
-      },
-      CallExpression(p, state) {
-        const callee = p.get('callee');
-        if (!callee.isIdentifier() || !callee.equals('name', 'require')) {
-          return;
-        }
+export function transformImportsInline({ types: t }) {
+    return {
+        visitor: {
+            ImportDeclaration(p, state) {
+                applyTransform(p, t, state, p.node.source.value, 'import');
+            },
+            CallExpression(p, state) {
+                const callee = p.get('callee');
+                if (!callee.isIdentifier() || !callee.equals('name', 'require')) {
+                    return;
+                }
 
-        const arg = p.get('arguments')[0];
-        if (!arg || !arg.isStringLiteral()) {
-          return;
-        }
+                const arg = p.get('arguments')[0];
+                if (!arg || !arg.isStringLiteral()) {
+                    return;
+                }
 
-        applyTransform(p, t, state, arg.node.value, 'require');
-      }
+                applyTransform(p, t, state, arg.node.value, 'require');
+            }
+        }
     }
-  }
 }
 
 export default transformImportsInline;
